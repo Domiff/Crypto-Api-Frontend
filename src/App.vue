8637d0820b5055a36055468bad65ff02e9c1b2ad
dmitriy <dmlegasy@gmail.com>
2026-02-23 19:47:58 +0300

    Add root component

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-if="$route.fullPath === '/'">
+        <main>
+            <RouterView/>
+        </main>
+    </div>
+
+    <div v-if="$route.fullPath === '/bitcoin'">
+        <main>
+            <RouterView/>
+        </main>
+    </div>
+
+    <div v-if="$route.fullPath === '/ethereum'">
+        <main>
+            <RouterView/>
+        </main>
+    </div>
+</template>
