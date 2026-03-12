222d4553f851ae51bf59fc9a087e62e40cfe9ae1
dmitriy levykin <dmlegasy@gmail.com>
2026-03-12 11:20:36 +0300

    Add component and styles for calendar

---
+++ src/components/calendar/Calendar.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import {ref} from "vue"
+    import {VueDatePicker} from "@vuepic/vue-datepicker"
+    import '@vuepic/vue-datepicker/dist/main.css'
+    import "@/assets/datepicker.css"
+
+    const date = ref()
+</script>
+
+<template>
+    <VueDatePicker v-model="date" />
+</template>
